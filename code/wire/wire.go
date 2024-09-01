2f20c653f5d05e574365f502d5cdda38ce1d6a95
bornjre <bornjre@gmail.com>
2024-09-01 11:30:51 +0545

    remove slog

--- code/wire/wire.go
+++ code/wire/wire.go
@@ -2,7 +2,7 @@ package wire
 
 import (
 	"encoding/binary"
-	"log/slog"
+	"log"
 
 	"github.com/k0kubun/pp"
 	"github.com/libp2p/go-libp2p/core/network"
@@ -10,12 +10,12 @@ import (
 
 func WritePacket(stream network.Stream, packet *Packet) error {
 
-	slog.Info("writePacket/1")
+	log.Println("writePacket/1")
 
 	// write packet type
 	_, err := stream.Write([]byte{packet.PType})
 	if err != nil {
-		slog.Info("writePacket/2")
+		log.Println("writePacket/2")
 		return err
 	}
 
@@ -26,7 +26,7 @@ func WritePacket(stream network.Stream, packet *Packet) error {
 	binary.BigEndian.PutUint32(intBytes, packet.Length)
 	_, err = stream.Write(intBytes)
 	if err != nil {
-		slog.Info("writePacket/3")
+		log.Println("writePacket/3")
 		return err
 	}
 
@@ -34,7 +34,7 @@ func WritePacket(stream network.Stream, packet *Packet) error {
 	binary.BigEndian.PutUint32(intBytes, packet.Offset)
 	_, err = stream.Write(intBytes)
 	if err != nil {
-		slog.Info("writePacket/4")
+		log.Println("writePacket/4")
 		return err
 	}
 
@@ -42,7 +42,7 @@ func WritePacket(stream network.Stream, packet *Packet) error {
 	binary.BigEndian.PutUint32(intBytes, packet.Total)
 	_, err = stream.Write(intBytes)
 	if err != nil {
-		slog.Info("writePacket/5")
+		log.Println("writePacket/5")
 		return err
 	}
 
@@ -50,7 +50,7 @@ func WritePacket(stream network.Stream, packet *Packet) error {
 	written, err := stream.Write(packet.Data)
 
 	if err != nil {
-		slog.Info("writePacket/6")
+		log.Println("writePacket/6")
 		return err
 	}
 
@@ -61,7 +61,7 @@ func WritePacket(stream network.Stream, packet *Packet) error {
 
 func ReadPacket(stream network.Stream) (*Packet, error) {
 
-	slog.Info("readPacket/1")
+	log.Println("readPacket/1")
 
 	packet := &Packet{}
 	intBytes := make([]byte, 4)
@@ -69,19 +69,19 @@ func ReadPacket(stream network.Stream) (*Packet, error) {
 	// read packet type
 	_, err := stream.Read(intBytes[:1])
 	if err != nil {
-		slog.Info("readPacket/2")
+		log.Println("readPacket/2")
 
 		return nil, err
 	}
 
-	slog.Info("readPacket/3")
+	log.Println("readPacket/3")
 
 	ptype := uint8(intBytes[0])
 
 	// read length
 	_, err = stream.Read(intBytes)
 	if err != nil {
-		slog.Info("readPacket/4")
+		log.Println("readPacket/4")
 		return nil, err
 	}
 
@@ -89,33 +89,33 @@ func ReadPacket(stream network.Stream) (*Packet, error) {
 
 	// read offset
 
-	slog.Info("readPacket/5")
+	log.Println("readPacket/5")
 
 	_, err = stream.Read(intBytes)
 	if err != nil {
-		slog.Info("readPacket/6")
+		log.Println("readPacket/6")
 		return nil, err
 	}
 	offset := binary.BigEndian.Uint32(intBytes)
 
-	slog.Info("readPacket/7")
+	log.Println("readPacket/7")
 
 	// read total
 	_, err = stream.Read(intBytes)
 	if err != nil {
-		slog.Info("readPacket/8")
+		log.Println("readPacket/8")
 		return nil, err
 	}
 	total := binary.BigEndian.Uint32(intBytes)
 
-	slog.Info("readPacket/9")
+	log.Println("readPacket/9")
 
 	// read data
 
 	dataBytes := make([]byte, length)
 	readSize, err := stream.Read(dataBytes)
 	if err != nil {
-		slog.Info("readPacket/10")
+		log.Println("readPacket/10")
 		return nil, err
 	}
 
@@ -127,7 +127,7 @@ func ReadPacket(stream network.Stream) (*Packet, error) {
 	packet.Total = total
 	packet.Data = dataBytes
 
-	slog.Info("readPacket/11")
+	log.Println("readPacket/11")
 
 	pp.Println("@read_data", packet)
 
